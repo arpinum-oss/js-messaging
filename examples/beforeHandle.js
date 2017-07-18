15b236c8aa3a4bd9b66944e736bab356e529f494
Michael Borde <michael.borde@ubimail.fr>
2017-07-18 08:30:55 +0200

    examples simplification

--- examples/beforeHandle.js
+++ examples/beforeHandle.js
@@ -16,11 +16,9 @@ bus.post({type: 'PrintText', payload: {text: 'Hello world'}});
 // HELLO-WORLD
 
 function withUpperCaseText(message) {
-  let payload = Object.assign({}, message.payload, {text: message.payload.text.toUpperCase()});
-  return Object.assign({}, message, {payload});
+  return message.updatePayload({text: message.payload.text.toUpperCase()});
 }
 
 function withoutSpaceInText(message) {
-  let payload = Object.assign({}, message.payload, {text: message.payload.text.replace(' ', '-')});
-  return Object.assign({}, message, {payload});
+  return message.updatePayload({text: message.payload.text.replace(' ', '-')});
 }
