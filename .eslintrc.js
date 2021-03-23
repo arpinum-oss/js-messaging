0f3db79f3efc41b322b83da7818ce8d63e227f0a
Michael Borde <michael.borde@ubimail.fr>
2021-03-23 11:29:26 +0100

    tests: linting js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
-// eslint-disable-next-line no-undef
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: { "@typescript-eslint/no-explicit-any": "off" },
+  overrides: [
+    {
+      files: ["*.js"],
+      env: {
+        node: true,
+      },
+      rules: { "@typescript-eslint/no-var-requires": "off" },
+    },
+  ],
 };
