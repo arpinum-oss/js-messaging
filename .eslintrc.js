b52b586e2ee35ddeb57c33322da8a2f1c652a378
Michael Borde <michael.borde@ubimail.fr>
2021-03-25 09:16:04 +0100

    tests: eslint ignore in .eslintrc.js instead of .eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: { "@typescript-eslint/no-explicit-any": "off" },
+  ignorePatterns: ["build", "coverage"],
   overrides: [
     {
       files: ["*.js"],
